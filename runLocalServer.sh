800b3e15ab08d2c597484047ee705019a3ebaa46
Manfred Moser <manfred@simpligility.com>
2016-03-03 15:29:02 -0800

    excluding and documenting local server script

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+# Start a webserver off the local directory, 
+# this enables debugging javascript with cookies as needed for the cross page accordion state persistence 
 python -m SimpleHTTPServer 9000 
