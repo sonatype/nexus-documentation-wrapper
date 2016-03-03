138a8c9611e0a19628c978443df1c84cd21c7c82
Manfred Moser <manfred@simpligility.com>
2016-03-03 15:51:30 -0800

    added docs

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Start a webserver off the local directory, 
-# this enables debugging javascript with cookies as needed for the cross page accordion state persistence 
+# this enables debugging javascript with cookies as needed for the cross page accordion state persistence
+ 
 python -m SimpleHTTPServer 9000 
