005208e6c42ffa3d42425a71cacc6a65268bcd67
Manfred Moser <manfred@simpligility.com>
2016-03-03 16:27:39 -0800

    cleaned and documented navigation script

--- js/navigation.js
+++ js/navigation.js
@@ -1,35 +1,37 @@
-function setCookie(key, value) {
-	  Cookies.set(key, value, { expires: 3 });
-  }
+// navigation.js
+// Manfred Moser
+// Requirements: jquery, js.cookie
 
-  function getCookie(key) {
-      var value = Cookies.get(key);
-      return value;
-  }
-  
-  $(document).ready(function($) {
-    // read status of all titles from cookie array, 
-    // if none found, set to expanded false
-    $('#accordion').find('.sidebarTitle').each(
-    		  function() {
-    			  showSection = getCookie(this.id);
-    			  if (showSection == "show" ) {
-    			        $(this).next().show();
-  			      } else {
-    			        //$(this).next().slideToggle('fast');
-    			        $(this).next().hide();
-    			    }
-    			}		
-    );
-    // expanded and contract as set in the array
-    
-	  // set onClick event for all titles
-	  $('#accordion').find('.sidebarTitle').click(function(){
-      if($(this).next().is(':visible')) {
-    	  setCookie(this.id, "hide")
-      } else {
-        setCookie(this.id, "show")
-      }
-      $(this).next().slideToggle('fast');
-    });
+// Accordion style navigation that keeps track of hide/show status for each section
+// via cookies so that each individual static page can read the state of the
+// section in the nav and display them correctly
+
+function setCookie(key, value) {
+	Cookies.set(key, value, { expires: 3 });
+}
+
+$(document).ready(function($) {
+  // iterate through titles, read cookie and display as set
+  $('#accordion').find('.sidebarTitle').each(function() {
+    showSection = Cookies.get(this.id);
+    if (showSection == "show" ) {
+      $(this).next().show();
+    } else {
+      $(this).next().hide();
+    }
   });
+  
+  // set onClick event for all titles
+  $('#accordion').find('.sidebarTitle').click(function() {
+    // if its visible and was clicked
+    if($(this).next().is(':visible')) {
+      // set it to hidden
+	  setCookie(this.id, "hide")
+    } else {
+      // otherwise set to be shown
+      setCookie(this.id, "show")
+    }
+    // always toggle on click
+    $(this).next().slideToggle('fast');
+  });
+});
