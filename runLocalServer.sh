396b5f35a4fee63d4a5cd170676125bfa5bac17c
Manfred Moser <manfred@simpligility.com>
2016-03-10 09:20:27 -0800

    added usage of env variable for location of deployment

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -2,5 +2,6 @@
 
 # Start a webserver off the local directory, 
 # this enables debugging javascript with cookies as needed for the cross page accordion state persistence
- 
+
+cd $NEXUS_DOCUMENTATION
 python -m SimpleHTTPServer 9000 
