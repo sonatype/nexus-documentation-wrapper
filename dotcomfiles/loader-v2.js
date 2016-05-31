93b6f75a5d2b268fa58bc4d0048a88600c58b4e7
Manfred Moser <manfred@simpligility.com>
2016-05-31 11:15:08 -0700

    adding page from .com site to use a shell after cleaning it up and emptying it out

---
+++ dotcomfiles/loader-v2.js
@@ -0,0 +1,94 @@
+(function() {
+
+    var __hs_cta_json = {"css":"a#cta_button_1958393_7d3ca23b-b390-4a91-9dbb-e23f19cbe581 {\n  -webkit-font-smoothing:antialiased; \n  cursor:pointer; \n  -moz-user-select:none; \n  -webkit-user-select:none; \n  -o-user-select:none; \n  user-select:none; \n  display:inline-block; \n  font-weight:bold; \n  text-align:center; \n  text-decoration:none; \n  font-family:sans-serif; \n  border-radius:5px; \n  background-color:rgb(44,45,46); \n  background-image:-webkit-linear-gradient(top, rgb(44,45,46), rgb(33,33,34)); \n  background-image :-moz-linear-gradient(top, rgb(44,45,46), rgb(33,33,34)); \n  box-shadow:inset 0px 1px rgb(55,56,57); \n  -webkit-box-shadow:inset 0px 1px rgb(55,56,57); \n  -moz-box-shadow:inset 0px 1px rgb(55,56,57); \n  color:rgb(255, 255, 255); \n  border:2px solid rgb(22,22,23); \n  text-shadow:0px -1px rgb(13,13,13); \n  line-height:1.5em; \n  padding:14px 26px; \nborder-radius: 32px;\nfont-family: \"Proxima Nova Semibold\"}\na#cta_button_1958393_7d3ca23b-b390-4a91-9dbb-e23f19cbe581:hover {\nbackground-color:rgb(48,49,50); \nbackground-image:-webkit-linear-gradient(top, rgb(48,49,50), rgb(33,33,34)); \nbackground-image :-moz-linear-gradient(top, rgb(48,49,50), rgb(33,33,34)); \nbox-shadow:inset 0px 1px rgb(59,60,62), 0px 1px 8px rgba(0, 0, 0, 0.3); \n-webkit-box-shadow:inset 0px 1px rgb(59,60,62), 0px 1px 8px rgba(0, 0, 0, 0.3); \n-moz-box-shadow:inset 0px 1px rgb(59,60,62), 0px 1px 8px rgba(0, 0, 0, 0.3); \ncolor:rgb(255,255,255); \nborder:2px solid rgb(26,27,27); \n}\na#cta_button_1958393_7d3ca23b-b390-4a91-9dbb-e23f19cbe581:active, #cta_button_1958393_7d3ca23b-b390-4a91-9dbb-e23f19cbe581:active:hover {\nbackground-color:rgb(33,33,34); \nbackground-image:-webkit-linear-gradient(top, rgb(33,33,34), rgb(44,45,46)); \nbackground-image :-moz-linear-gradient(top, rgb(33,33,34), rgb(44,45,46)); \nbox-shadow:inset 0px 1px 10px rgba(0, 0, 0, 0.5); \n-webkit-box-shadow:inset 0px 1px 10px rgba(0, 0, 0, 0.5); \n-moz-box-shadow:inset 0px 1px 10px rgba(0, 0, 0, 0.5); \ncolor:rgb(244,244,244); \n}\n\n","image":false,"_id":"false","raw_html":"<a id=\"cta_button_1958393_7d3ca23b-b390-4a91-9dbb-e23f19cbe581\" class=\"cta_button sonatype-secondary-cta\" href=\"//cta-service-cms2.hubspot.com/ctas/v2/public/cs/c/?cta_guid=7d3ca23b-b390-4a91-9dbb-e23f19cbe581&placement_guid=e084f104-9408-4e3d-80b1-2f49baab013f&portal_id=1958393&redirect_url=APefjpE7x8iTrztISGuZl3q4u-xxaHkPQTAuJxKkUdB8h9i-n0RUrGUejiRwbjKG8WFs_MwR2DQZUCDSO7zeBDLn2PiZy2Gi5A42UkuUcRW738PY9xi7vn_gMkjI75eGxxTlaTFoj5EkNNCkjHiVaBptItV11b502u5VTfpCIraKTultlpU2UIA61VsKnD5wJSqc_M1CMLR777p1Srmj1csNTMZWfxABSQEGj1TKdBrcaox7oX8g94X6AyceSGawmB_uU2cdTylCeus_k60N9pHBO7iTE8YbLpcxK6zXAjRPJzJP08zCF6c&hsutk=5278035f300692568c31299b9604d3d6&utm_referrer=http%3A%2F%2Fwww.sonatype.com%2Fnexus-lifecycle&canon=http%3A%2F%2Fwww.sonatype.com%2Fnexus-lifecycle\"  style=\"/*hs-extra-styles*/\" cta_dest_link=\"http://www.sonatype.com/nexus-lifecycle-tour-software-supply-chain-automation-sonatype\" title=\"Take a Tour\"> <div style=\"text-align: center;\">Take a Tour</div> </a> ","image_html":"<a id=\"cta_button_1958393_7d3ca23b-b390-4a91-9dbb-e23f19cbe581\" class=\"cta_button\" href=\"//cta-service-cms2.hubspot.com/ctas/v2/public/cs/c/?cta_guid=7d3ca23b-b390-4a91-9dbb-e23f19cbe581&placement_guid=e084f104-9408-4e3d-80b1-2f49baab013f&portal_id=1958393&redirect_url=APefjpE7x8iTrztISGuZl3q4u-xxaHkPQTAuJxKkUdB8h9i-n0RUrGUejiRwbjKG8WFs_MwR2DQZUCDSO7zeBDLn2PiZy2Gi5A42UkuUcRW738PY9xi7vn_gMkjI75eGxxTlaTFoj5EkNNCkjHiVaBptItV11b502u5VTfpCIraKTultlpU2UIA61VsKnD5wJSqc_M1CMLR777p1Srmj1csNTMZWfxABSQEGj1TKdBrcaox7oX8g94X6AyceSGawmB_uU2cdTylCeus_k60N9pHBO7iTE8YbLpcxK6zXAjRPJzJP08zCF6c&hsutk=5278035f300692568c31299b9604d3d6&utm_referrer=http%3A%2F%2Fwww.sonatype.com%2Fnexus-lifecycle&canon=http%3A%2F%2Fwww.sonatype.com%2Fnexus-lifecycle\"  cta_dest_link=\"http://www.sonatype.com/nexus-lifecycle-tour-software-supply-chain-automation-sonatype\"> <img id=\"hs-cta-img-e084f104-9408-4e3d-80b1-2f49baab013f\" class=\"hs-cta-img sonatype-secondary-cta\" style=\"border-width: 0px; /*hs-extra-styles*/\" mce_noresize=\"1\" alt=\"Take a Tour\" src=\"http://cdn2.hubspot.net/hubshot/16/05/04/ad5fec45-d94b-40ee-bdee-c56eb8653627.png\" /> </a> ","is_image":false,"_class":"hs-cta-e084f104-9408-4e3d-80b1-2f49baab013f"};
+    var __hs_cta = {};
+
+    __hs_cta.drop = function() {
+        var is_legacy = document.getElementById('hs-cta-ie-element') && true || false,
+            html = __hs_cta_json.image_html,
+            tags = __hs_cta.getTags(),
+            is_image = __hs_cta_json.is_image,
+            parent, _style;
+
+        if (!is_legacy && !is_image) {
+            parent = (document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]);
+
+            _style = document.createElement('style');
+            parent.insertBefore(_style, parent.childNodes[0]);
+            try {
+                default_css = ".hs-cta-wrapper p, .hs-cta-wrapper div { margin: 0; padding: 0; }";
+                cta_css = default_css + " " + __hs_cta_json.css;
+                // http://blog.coderlab.us/2005/09/22/using-the-innertext-property-with-firefox/
+                _style[document.all ? 'innerText' : 'textContent'] = cta_css;
+            } catch (e) {
+                // addressing an ie9 issue
+                _style.styleSheet.cssText = cta_css;
+            }
+
+            html = __hs_cta_json.raw_html;
+        }
+
+        for (var i = 0; i < tags.length; ++i) {
+
+            var tag = tags[i];
+            var images = tag.getElementsByTagName('img');
+            var cssText = "";
+            var align = "";
+            for (var j = 0; j < images.length; j++) {
+                align = images[j].align;
+                images[j].style.border = '';
+                images[j].style.display = '';
+                cssText = images[j].style.cssText;
+            }
+
+            if (align == "right") {
+                tag.style.display = "block";
+                tag.style.textAlign = "right";
+            } else if (align == "middle") {
+                tag.style.display = "block";
+                tag.style.textAlign = "center";
+            }
+
+            tag.innerHTML = html.replace('/*hs-extra-styles*/', cssText);
+            tag.style.visibility = 'visible';
+            tag.setAttribute('data-hs-drop', 'true');
+            window.hbspt && hbspt.cta && hbspt.cta.afterLoad && hbspt.cta.afterLoad();
+        }
+
+        return tags;
+    };
+
+    __hs_cta.getTags = function() {
+        var allTags, check, i, divTags, spanTags,
+            tags = [];
+            if (document.getElementsByClassName) {
+                allTags = document.getElementsByClassName(__hs_cta_json._class);
+                check = function(ele) {
+                    return (ele.nodeName == 'DIV' || ele.nodeName == 'SPAN') && (!ele.getAttribute('data-hs-drop'));
+                };
+            } else {
+                allTags = [];
+                divTags = document.getElementsByTagName("div");
+                spanTags = document.getElementsByTagName("span");
+                for (i = 0; i < spanTags.length; i++) {
+                    allTags.push(spanTags[i]);
+                }
+                for (i = 0; i < divTags.length; i++) {
+                    allTags.push(divTags[i]);
+                }
+
+                check = function(ele) {
+                    return (ele.className.indexOf(__hs_cta_json._class) > -1) && (!ele.getAttribute('data-hs-drop'));
+                };
+            }
+            for (i = 0; i < allTags.length; ++i) {
+                if (check(allTags[i])) {
+                    tags.push(allTags[i]);
+                }
+            }
+        return tags;
+    };
+
+    // need to do a slight timeout so IE has time to react
+    setTimeout(__hs_cta.drop, 10);
+}());
