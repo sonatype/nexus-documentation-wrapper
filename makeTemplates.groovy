e50d28455a71482cb9e4e8e87eb69b7bfb431bbe
Manfred Moser <manfred@simpligility.com>
2016-09-15 16:40:20 -0700

    Adaptations for IQ docs
    - pulled full nav HTML into template groovy script
    - better logging in template groovy script
    - changed Other Resources to be above ToC links

--- makeTemplates.groovy
+++ makeTemplates.groovy
@@ -1,7 +1,10 @@
 def newline = System.getProperty("line.separator")
 
+def pre = "makeTemplate: "
+println "$pre Starting"
+
 wrapperDir = new File(getClass().protectionDomain.codeSource.location.path).parent + File.separator
-println "Running script in $wrapperDir"
+println "$pre Running script in $wrapperDir"
 //To get the script file path
 
 //scriptFile = getClass().protectionDomain.codeSource.location.path
@@ -21,8 +24,26 @@ new PrintWriter(wrapperDir + 'part-nav.html').close()
 // open the file for writing
 File nav = new File(wrapperDir + 'part-nav.html')
 
+nav.append("<div class=\"sidebarTitle\" id=\"nxDocsSectNav\"><b>\${product} \${version}</b></div>")
+nav.append("<div class=\"sidebarSection\">")
+
+
+def linkCount = 0
 doc.depthFirst().collect { it }.findAll { it.name() == "span" && it.@class.text()=="chapter"}.each {
     nav.append("<div class=\"sidebar\"><a href=\"\${toindex}nexus-book/reference/${it.a.@href}\">${it}</a></div>${newline}")
+    println "$pre added $it"
+    linkCount++
+}
+
+println "$pre Added $linkCount links"
+
+nav.append("</div>")
+// nav.append("<br />")
+// nav.append("</div>")
+  
+if (linkCount == 0) {
+  new PrintWriter(wrapperDir + 'part-nav.html').close()
+  println "$pre Flushed $nav since no links added."
 }
 
 // create a new empty file (or overwrite existing with empty
@@ -36,6 +57,8 @@ def templatePieces = [wrapperDir + 'part-header2nav.html',
                       wrapperDir + 'part-body2footer.html']
 templatePieces.each{ newTemplate.append(new File(it).getText()) }
 
+println "$pre Assembled $newTemplate"
+
 // create a new empty file (or overwrite existing with empty
 new PrintWriter(wrapperDir + 'search.html').close()
 
@@ -46,7 +69,8 @@ def searchPieces = [wrapperDir + 'part-header2nav.html',
                     wrapperDir + 'part-bodySearch.html', 
                     wrapperDir + 'part-body2footer.html']
 searchPieces.each{ newSearch.append(new File(it).getText()) }
+println "$pre Assembled $newSearch"
 
 // Need to add the deletion of the temporary nav.html file or ignore it in the template.py file.. otherwise it fails, maybe also ensure the other files dont leak into the output 
 
-println "Done"
+println "$pre Done"
